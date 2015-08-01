d659d02b376d67d57a276ef3bf1716b45234aaad
Pascal Craponne <pascal@craponne.fr>
2015-08-01 21:49:41 +0200

    Updated version

--- ArxOne.Ftp/Properties/AssemblyInfo.cs
+++ ArxOne.Ftp/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("1.7")]
+[assembly: AssemblyVersion("1.7.1")]
 
 [assembly: CLSCompliant(true)]
 
